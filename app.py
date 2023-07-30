62a2bf78d271968f2f8a62273997a3f6d0a81ed4
Jeeva95 <josephjeeva20@gmail.com>
2023-07-30 16:41:12 +0530

    modified commit

--- app.py
+++ app.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def welcome():
     return 'welcome to the flask'
 
+@app.route('/cal', methods =['Get'])
+def math_operations():
+    operation = request.json['operation']
+    num_1 = request.json['num_1']
+    num_2 = request.json['num_2']
+    
+    if operation == 'add':
+        result = num_1+num_2
+    elif operation == 'multiply' :
+        result = num_1*num_2
+    elif operation == 'division':
+        result = num_1/num_2
+    else :
+        result = num_1-num_2
 if __name__=='__main__':
     app.run()
